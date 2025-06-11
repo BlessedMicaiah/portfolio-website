9e307d9e71c7ef90c39a53665652fbcb0fc414bd
Faithology <43255094+BlessedMicaiah@users.noreply.github.com>
2025-06-10 23:00:57 -0500

    Initial commit for Heroku deployment

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
 const express = require('express');
-const compression = require('compression');
 const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-// Enable compression for all routes
-app.use(compression());
 
 // Serve static files
 app.use(express.static(__dirname));
 
-// Handle all routes by sending the index.html file
+// For any route that doesn't match a static file, serve index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
